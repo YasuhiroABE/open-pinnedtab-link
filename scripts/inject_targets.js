e7b7e8f1e0f393d27e083497fff35e35fc0f018b
Yasuhiro ABE <yasundial@users.sourceforge.jp>
2011-03-29 05:49:46 +0900

    Initial commit.

---
+++ scripts/inject_targets.js
@@ -0,0 +1,8 @@
+
+// Copyright (C) 2011 Yasuhiro ABE <yasu@yasundial.org>
+
+var al = document.getElementsByTagName("a");
+for(var i=0; i<al.length; i++){
+  al[i].setAttribute("target","_blank");
+}
+
