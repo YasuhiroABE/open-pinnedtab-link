b4335211d420f4f796c57e4f90e7e511d9f2f729
Yasuhiro ABE <yasundial@users.sourceforge.jp>
2011-03-29 15:42:04 +0900

    Using localStorage to store the pinned tab status.

--- scripts/init.js
+++ scripts/init.js
@@ -2,8 +2,9 @@
 // Copyright (C) 2011 Yasuhiro ABE <yasu@yasundial.org>
 // Licensed under the Apache License, Version 2.0 (the "License")
 
-var al = document.getElementsByTagName("a");
-for(var i=0; i < al.length; i++){
-    al[i].setAttribute("target","_blank");
+function getHostname(href) {
+    var anchor = document.createElement("a");
+    anchor.href = href;
+    return anchor.hostname;
 }
 
