8c04aa760297d05372d39b3b509a3df30b3b37e4
Yasuhiro ABE <yasundial@users.sourceforge.jp>
2011-03-31 01:55:20 +0900

    Implemented the ignore to inject the "target=_blank" to anchor when it is the same site.

--- scripts/inject_targets.js
+++ scripts/inject_targets.js
@@ -2,8 +2,5 @@
 // Copyright (C) 2011 Yasuhiro ABE <yasu@yasundial.org>
 // Licensed under the Apache License, Version 2.0 (the "License")
 
-var al = document.getElementsByTagName("a");
-for(var i=0; i < al.length; i++){
-    al[i].setAttribute("target","_blank");
-}
+var al = document.getElementsByTagName("a");for(var i=0; i < al.length; i++){    al[i].setAttribute("target","_blank");}
 
